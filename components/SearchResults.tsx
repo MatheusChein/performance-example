92047b40fe1f90f31c0cebe29b6b4fe876d1d969
Matheus Chein <matheus.chein@fabric.inc>
2022-01-21 18:12:02 -0300

    implementing virtualization for the list

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from 'react-virtualized'
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -14,17 +15,29 @@ interface SearchResultsProps {
 
 export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
 
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem 
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>Total: {totalPrice}</h2>
 
-      {results.map(item => (
-        <ProductItem 
-          key={item.id} 
-          product={item}
-          onAddToWishList={onAddToWishList}
-        />
-      ))}
+      <List
+        height={500}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
